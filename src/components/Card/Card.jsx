b2298b1c7e5cb0b2595f0e9b0214c4899757facb
unknown <chiriac_razvan@ymail.com>
2022-07-28 18:51:15 +0300

    added increase number of points on Add Points button press functionality

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,24 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Card.styles.css";
 
 const Card = ({ cards }) => {
-  const dataDescendentSortedByPoints = cards.sort(
-    (pilotPoints, otherPilotPoints) =>
-      otherPilotPoints.points - pilotPoints.points
-  );
+  //const [isHovering, setIsHovering] = useState(false);
+  const [cardsList, setCardsList] = useState([]);
+
+  const sortByPoints = (cards) =>
+    cards.sort((pilot, otherPilot) => otherPilot.points - pilot.points);
+
+  console.log(cardsList);
+  console.log(sortByPoints(cardsList));
+
+  useEffect(() => setCardsList(cards), []);
+
+  const addPoints = (index) => {
+    const newCard = { ...cardsList[index] };
+    let currentCardPoints = newCard.points;
+    const newPoints = parseInt(currentCardPoints) + 1;
+    newCard.points = newPoints;
+    setCardsList([
+      ...cardsList.slice(0, index),
+      newCard,
+      ...cardsList.slice(index + 1, cardsList.length),
+    ]);
+  };
+
+  //   const handleMouseEnter = () => {
+  //     setIsHovering(true);
+  //   };
+
+  //   const handleMouseLeave = () => {
+  //     setIsHovering(false);
+  //   };
   return (
     <>
-      {dataDescendentSortedByPoints.map(
+      {sortByPoints(cardsList).map(
         (
           { firstName, lastName, number, team, points, image, country, hex },
           index
         ) => (
           <article className="card" key={number}>
-            <div className="border">
+            <div
+              className="border"
+              //   onMouseEnter={handleMouseEnter}
+              //   onMouseLeave={handleMouseLeave}
+              //   style={{
+              //     borderTopColor: isHovering ? hex : "",
+              //     borderRightColor: isHovering ? hex : "",
+              //   }}
+            >
               <div className="card-header">
                 <p className="rank">{index + 1}</p>
                 <span className="points-and-button">
-                  <button type="button" className="add-points">
+                  <button
+                    type="button"
+                    className="add-points"
+                    onClick={() => addPoints(index)}
+                  >
                     Add PTS
                   </button>
                   <p className="points-text">{points}</p>
